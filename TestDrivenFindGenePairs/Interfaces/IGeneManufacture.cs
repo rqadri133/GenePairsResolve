2475db0fbe9e09e6fa6e2699321faab1cd1323ec
Syed Qadri <rqadri133@gmail.com>
2023-08-20 02:43:35 -0400

    This was an assignment questions i have only replied with code models

--- TestDrivenFindGenePairs/Interfaces/IGeneManufacture.cs
+++ TestDrivenFindGenePairs/Interfaces/IGeneManufacture.cs
@@ -5,21 +5,23 @@ using System;
 
 namespace TestDrivenFindGenePairs.interfaces 
 {
-    interface IGeneManufacture<T>  where T: IProtien  
+    interface IGeneManufacture<T>  where T: ProteinContent  
     {
         // EPIC
-        // Finding all pattersn 
         // Finding Lab Result files Samples 
-        // Finding Researched infected Pattern from Lab / disclosure Court Proceddings Required
         // Finding possible strands 
-        // Call NIH ( National Isntitute of Health) , negotitaions required to get sample data 
          public string MergeReplaceGene(List<Gene> source , List<Gene> healthyGene);
          // We will find mutiple protien strcuture patterns  
          public string FindProtienVirusStructure(T t);
+
+         public string RemoveReplaceVirusStucture(T t);
          public string ReplaceInfectedChromosome(List<Gene> source, StringBuilder genefile); 
          
 
     }
-    
+
+    internal class ProteinContent
+    {
+    }
 }
 
